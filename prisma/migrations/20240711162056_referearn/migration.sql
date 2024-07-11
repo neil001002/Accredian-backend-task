44e7b15aefbde88729d6e9d1f2ace276dfae383f
neil001002 <neil001002@gmail.com>
2024-07-11 22:04:14 +0530

    added database prisma vercel config

---
+++ prisma/migrations/20240711162056_referearn/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE `Referral` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `referrerName` VARCHAR(191) NOT NULL,
+    `referrerEmail` VARCHAR(191) NOT NULL,
+    `referrerPhone` VARCHAR(191) NOT NULL,
+    `refereeName` VARCHAR(191) NOT NULL,
+    `refereeEmail` VARCHAR(191) NOT NULL,
+    `refereePhone` VARCHAR(191) NOT NULL,
+    `course` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `Referral_referrerEmail_key`(`referrerEmail`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use("/api/referrals", referralRoutes);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
